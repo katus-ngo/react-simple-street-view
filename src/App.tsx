82aff2a07efeb4a9ec0742d2f90123ff87d5d7b5
katus <truongnxt.1512@gmail.com>
2021-11-16 09:11:20 +0700

    feat: 🎸 Basic feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useMemo, useRef, useState} from 'react';
+import './App.scss';
+import {scenes} from './mock/scenes.json'
+import {TScenes, TScenesID} from "./types";
+import {BsArrowDownCircle, BsArrowLeftCircle, BsArrowRightCircle, BsArrowUp} from 'react-icons/bs';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const hitZonesRef = useRef<any>([]);
+
+    const [step, setStep] = useState<TScenesID>(scenes[0].id)
+    const [history, setHistory] = useState<TScenesID[]>([])
+
+
+    const selectedScenes = useMemo((): TScenes | undefined => {
+        return scenes.find(s => s.id === step)
+    }, [step])
+
+    const onClickHitZone = (id: TScenesID) => {
+        setStep(id)
+    }
+
+    useEffect(() => {
+        hitZonesRef.current = []
+    }, [step])
+
+    return (
+        <div className="app">
+            {
+                !!selectedScenes && (
+                    <div key={selectedScenes.id}
+                         className={"app__picture-container"}
+                         style={{}}>
+                        <div className={"app__picture--visible"}
+                             style={{
+                                 backgroundImage: `url(${selectedScenes.imageUrl})`,
+                                 top: selectedScenes.mainPosition.y,
+                                 left: `-${selectedScenes.mainPosition.x}`
+                             }}
+                        >
+                            {
+                                selectedScenes.hitZones.map((hitZone, index) => (
+                                    <div key={index}
+                                         ref={el => (hitZonesRef.current = [...hitZonesRef.current, el])}
+                                         className={"app__hit-zone"}
+                                         onClick={() => onClickHitZone(hitZone.goTo)}
+                                         style={{
+                                             top: hitZone.y,
+                                             left: hitZone.x
+                                         }}
+                                    >
+                                        <BsArrowUp className={"app__hit-icon"}/>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                        <img className={"app__picture--hidden"}
+                             src={selectedScenes.imageUrl} alt={''}
+                        />
+                    </div>
+                )
+            }
+
+            <div className={'app__view-action app__view-action--left'}>
+                <BsArrowLeftCircle className={"app__view-action-icon"}/>
+            </div>
+
+            <div className={'app__view-action app__view-action--right'}>
+                <BsArrowRightCircle className={"app__view-action-icon"}/>
+            </div>
+
+            <div className={'app__view-action app__view-action--back'}>
+                <BsArrowDownCircle className={"app__view-action-icon"}/>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,20 @@ export type TWindowDimensions = {
     width: number,
     height: number
 }
+export type TScenesID = number
+
+export type THitZone = {
+    x: string,
+    y: string,
+    goTo: TScenesID,
+}
+
+export type TScenes = {
+    id: TScenesID,
+    imageUrl: string,
+    hitZones: THitZone[],
+    mainPosition: {
+        x: string,
+        y: string
+    }
+}

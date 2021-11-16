98427b54cce9a2b5588f829a98de11b51efd2174
katus <truongnxt.1512@gmail.com>
2021-11-16 14:13:50 +0700

    feat: 🎸 Style & behavior view action

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,40 @@ import './App.scss';
 import {scenes} from './mock/scenes.json'
 import {TScenes, TScenesID} from "./types";
 import {BsArrowDownCircle, BsArrowLeftCircle, BsArrowRightCircle, BsArrowUp} from 'react-icons/bs';
+import clsx from 'clsx';
 
 function App() {
 
-    const hitZonesRef = useRef<any>([]);
+    let hitZonesRef = useRef<any[]>([]);
+    let viewActionRef = useRef<any[]>([]);
 
     const [step, setStep] = useState<TScenesID>(scenes[0].id)
     const [history, setHistory] = useState<TScenesID[]>([])
-
+    const [showViewAction, setShowViewAction] = useState(true)
 
     const selectedScenes = useMemo((): TScenes | undefined => {
         return scenes.find(s => s.id === step)
     }, [step])
 
     const onClickHitZone = (id: TScenesID) => {
+        hitZonesRef.current = []
         setStep(id)
     }
 
     useEffect(() => {
-        hitZonesRef.current = []
-    }, [step])
+        function handleClickOutside(event: MouseEvent) {
+            if ((hitZonesRef?.current?.concat(viewActionRef.current)).every((ref) => !(ref && ref?.contains(event.target)))) {
+                setShowViewAction(s => !s)
+                return
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside);
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [])
 
     return (
         <div className="app">
@@ -41,9 +55,9 @@ function App() {
                             {
                                 selectedScenes.hitZones.map((hitZone, index) => (
                                     <div key={index}
-                                         ref={el => (hitZonesRef.current = [...hitZonesRef.current, el])}
+                                         ref={(ref) => (hitZonesRef.current[index] = ref)}
                                          className={"app__hit-zone"}
-                                         onClick={() => onClickHitZone(hitZone.goTo)}
+                                         onClick={(e) => onClickHitZone(hitZone.goTo)}
                                          style={{
                                              top: hitZone.y,
                                              left: hitZone.x
@@ -61,16 +75,28 @@ function App() {
                 )
             }
 
-            <div className={'app__view-action app__view-action--left'}>
-                <BsArrowLeftCircle className={"app__view-action-icon"}/>
+            <div className={clsx('app__view-action', 'app__view-action--left', showViewAction && 'app__view-action--left--show')}>
+                <span className={'app__view-action-icon-container'}
+                      ref={(ref) => (viewActionRef.current = [...viewActionRef.current, ref])}
+                >
+                    <BsArrowLeftCircle className={"app__view-action-icon"}/>
+                </span>
             </div>
 
-            <div className={'app__view-action app__view-action--right'}>
-                <BsArrowRightCircle className={"app__view-action-icon"}/>
+            <div className={clsx('app__view-action', 'app__view-action--right', showViewAction && 'app__view-action--right--show')}>
+                <span className={'app__view-action-icon-container'}
+                      ref={(ref) => (viewActionRef.current = [...viewActionRef.current, ref])}
+                >
+                    <BsArrowRightCircle className={"app__view-action-icon"}/>
+                </span>
             </div>
 
-            <div className={'app__view-action app__view-action--back'}>
-                <BsArrowDownCircle className={"app__view-action-icon"}/>
+            <div className={clsx('app__view-action', 'app__view-action--back', showViewAction && 'app__view-action--back--show')}>
+                <span className={'app__view-action-icon-container'}
+                      ref={(ref) => (viewActionRef.current = [...viewActionRef.current, ref])}
+                >
+                    <BsArrowDownCircle className={"app__view-action-icon"}/>
+                </span>
             </div>
 
         </div>
